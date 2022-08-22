9553df69f95b47f27a239900ad8ea1c42c573116
Martin <martyminion@Martin>
2022-08-22 21:33:14 +0300

    Set up project, updated functions and tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,126 @@
-use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::borsh::{self, BorshDeserialize,BorshSerialize,Debug};
 use near_sdk::near_bindgen;
+
+#[derive(BorshDeserialize, BorshSerialize, Debug, PartialEq)]
+
+enum NetOptionsPackages { 
+    None,
+    Bronze,
+    Silver,
+    Gold,
+}
+
+
+impl Default for NetOptionsPackages{
+    fn default() -> Self {
+        NetOptionsPackages::None
+    }
+} 
 
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
-pub struct Contract {
+pub struct Customers {
     // SETUP CONTRACT STATE
+    name: String,
+    phone_number: String,
+    house_number: String,
+    net_option: NetOptionsPackages,
+    date_paid: String,
+    due_date: String,
 }
 
 #[near_bindgen]
-impl Contract {
+#[derive(Default, BorshDeserialize, BorshSerialize)]
+pub struct NetOptions {
+    customers: Vec<Customers>,
+}
+
+
+impl Default for NetOptions{
+    fn default() -> Self {
+        NetOptions {customers: vec![]}
+    }
+}
+
+#[near_bindgen]
+impl NetOptions {
     // ADD CONTRACT METHODS HERE
+    fn add_customer(
+        &mut self,
+        name:String,
+        phone: String,
+        house_number: String,
+        start: String,
+        due: String,
+        package: String,
+    ) {
+        let _bronze = String::from("small");
+        let _silver = String::from("small");
+        let _gold = String::from("small");
+        let cs = customers {
+            name: name,
+            phone_number:phone,
+            house_number:house_number,
+            net_option: match package {
+                _bronze => NetOptionsPackages::Bronze,
+                _silver => NetOptionsPackages::Silver,
+                _gold => NetOptionsPackages::Gold,
+                _ => NetOptionsPackages::None,
+            },
+            date_paid: start,
+            due_date: due,
+
+        };
+        self.customers.push(cs);
+    }
+    fn update_customer(
+        &mut self,
+        phone:String,
+        package: String,
+        date_paid: String,
+        date_due: String,
+    ){
+        let _bronze = "bronze".to_string();
+        let _silver = "silver".to_string();
+        let _gold = "gold".to_string();
+
+        for customer in self.customers.iter_mut() {
+            if customer.phone_number == phone {
+               
+                customer.date_paid = date_paid;
+                customer.due_date = date_due;
+                let x = match &package {
+                _bronze => NetOptionsPackages::Bronze,
+                _silver => NetOptionsPackages::Silver,
+                _gold => NetOptionsPackages::Gold,
+                _=> NetOptionsPackages::None,
+
+                };
+            }
+
+        }
+    }
+    fn get_customer(&self, phone: String) -> Option<&Customers>{
+        let mut a_customer :Option<&Customers> = None;
+
+        for elem in self.customers.iter() {
+            if elem.phone_number == phone {
+                a_customer = Some(elem);
+            }
+        }
+        return a_customer;
+    }
+    fn all_customer(&self) -> Vec<&Customers> {
+        let mut tmp : Vec<&Customers> = vec![];
+
+        for item in self.customers.iter()  {
+            tmp.push(item);
+        }
+        return tmp;
+    }
+
+
+
 }
 
 /*
@@ -35,4 +146,32 @@ mod tests {
     }
 
     // TESTS HERE
+    #[test]
+    fn add_customer() {
+        let mut app = NetOptions::default();
+        app.add_customer("kenn".to_string(), "111".to_string(), "001".to_string(), "21/07/2022".to_string(), "21/08/2022".to_string(), "bronze".to_owned());
+
+    assert_eq!(app.customers.len(), 1)
+    }
+    #[test]
+   fn update_customer(){
+    let mut  app = NetOptions::default();
+    
+    app.add_customer("kenn".to_string(), "111".to_string(), "001".to_string(), "21/07/2022".to_string(), "21/08/2022".to_string(), "bronze".to_owned());
+    //update Customer
+    app.update_customer("111".to_string(),"gold".to_string(),"23/06/2022".to_string(), "23/07/2022".to_string());
+
+    let red = app.get_customer("111".to_owned());
+        match red {
+            Some(yell)=>{
+            assert_eq!(yell.net_option,NetOptionsPackages::Medium)
+            } 
+            None=>{
+            panic!("Customer Not Found")
+    
+                }    
+        }
+    }
+
+
 }
