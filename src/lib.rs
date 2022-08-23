b4a9bf8b15347dcda3b5debab52ce1e08f398e3a
Martin <martyminion@Martin>
2022-08-23 20:21:45 +0300

    Working code.. hurray

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,27 @@
-use near_sdk::borsh::{self, BorshDeserialize,BorshSerialize,Debug};
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+
 use near_sdk::near_bindgen;
 
-#[derive(BorshDeserialize, BorshSerialize, Debug, PartialEq)]
-
-enum NetOptionsPackages { 
+#[derive(BorshDeserialize, BorshSerialize, Debug)]
+#[derive(PartialEq)]
+enum NetOptionsPackages {
     None,
     Bronze,
     Silver,
     Gold,
 }
 
-
-impl Default for NetOptionsPackages{
+impl Default for NetOptionsPackages {
     fn default() -> Self {
         NetOptionsPackages::None
     }
-} 
+}
+
+
 
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
 pub struct Customers {
-    // SETUP CONTRACT STATE
     name: String,
     phone_number: String,
     house_number: String,
@@ -30,85 +31,54 @@ pub struct Customers {
 }
 
 #[near_bindgen]
-#[derive(Default, BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize)]
 pub struct NetOptions {
     customers: Vec<Customers>,
 }
 
-
-impl Default for NetOptions{
+impl Default for NetOptions {
     fn default() -> Self {
-        NetOptions {customers: vec![]}
+        NetOptions { customers: vec![] }
     }
 }
 
 #[near_bindgen]
 impl NetOptions {
-    // ADD CONTRACT METHODS HERE
     fn add_customer(
         &mut self,
-        name:String,
+        name: String,
         phone: String,
         house_number: String,
         start: String,
         due: String,
         package: String,
     ) {
-        let _bronze = String::from("small");
-        let _silver = String::from("small");
-        let _gold = String::from("small");
-        let cs = customers {
-            name: name,
-            phone_number:phone,
-            house_number:house_number,
-            net_option: match package {
-                _bronze => NetOptionsPackages::Bronze,
-                _silver => NetOptionsPackages::Silver,
-                _gold => NetOptionsPackages::Gold,
-                _ => NetOptionsPackages::None,
-            },
-            date_paid: start,
-            due_date: due,
-
-        };
-        self.customers.push(cs);
-    }
-    fn update_customer(
-        &mut self,
-        phone:String,
-        package: String,
-        date_paid: String,
-        date_due: String,
-    ){
         let _bronze = "bronze".to_string();
         let _silver = "silver".to_string();
         let _gold = "gold".to_string();
+        
+        let mut _option: Option<NetOptionsPackages>=None;
 
-        for customer in self.customers.iter_mut() {
-            if customer.phone_number == phone {
-               
-                customer.date_paid = date_paid;
-                customer.due_date = date_due;
-                let x = match &package {
-                _bronze => NetOptionsPackages::Bronze,
-                _silver => NetOptionsPackages::Silver,
-                _gold => NetOptionsPackages::Gold,
-                _=> NetOptionsPackages::None,
-
-                };
-            }
-
+        if package.eq(&_bronze){
+            _option=Some(NetOptionsPackages::Bronze)
+        } else if package.eq(&_silver){
+            _option=Some(NetOptionsPackages::Silver)
+        } else if package.eq(&_gold){
+            _option=Some(NetOptionsPackages::Gold)
+        } else{
+            _option=Some(NetOptionsPackages::None)
         }
-    }
-    fn get_customer(&self, phone: String) -> Option<&Customers>{
-        let mut a_customer :Option<&Customers> = None;
 
-        for elem in self.customers.iter() {
-            if elem.phone_number == phone {
-                a_customer = Some(elem);
-            }
-        }
-        return a_customer;
+        let cs = Customers {
+            name: name,
+            phone_number: phone,
+            house_number: house_number,
+            date_paid: start,
+            due_date: due,
+            net_option: _option.unwrap(),
+            
+        };
+        self.customers.push(cs);
     }
     fn all_customer(&self) -> Vec<&Customers> {
         let mut tmp : Vec<&Customers> = vec![];
@@ -119,8 +89,59 @@ impl NetOptions {
         return tmp;
     }
 
+    fn get_customer(&self, phone: String) -> Option<&Customers> {
+
+        let mut a_customer :Option<&Customers> = None;
+
+        for elem in self.customers.iter() {
+            if elem.phone_number == phone{
+                a_customer = Some(elem);
+            }
+        }
+
+        return a_customer;
+    }
+
+    fn update_customer(
+        &mut self,
+        phone: String,
+        package: String,
+        date_paid: String,
+        date_due: String,
+    ) {
+
+        let _bronze = "bronze".to_string();
+        let _silver = "silver".to_string();
+        let _gold = "gold".to_string();
+
+        let mut _option: Option<NetOptionsPackages>=None;
+
+        if package.eq(&_bronze){
+            _option=Some(NetOptionsPackages::Bronze)
+        } else if package.eq(&_silver){
+            _option=Some(NetOptionsPackages::Silver)
+        } else if package.eq(&_gold){
+            _option=Some(NetOptionsPackages::Gold)
+        } else{
+            _option=Some(NetOptionsPackages::None)
+        }
+
+        for customer in self.customers.iter_mut() {
+            if customer.phone_number == phone {
+               
+                customer.date_paid = date_paid;
+                customer.due_date = date_due;
 
 
+              let x = _option.unwrap();
+
+                
+                customer.net_option = x;
+                
+                break;
+            }
+        }
+    }
 }
 
 /*
@@ -134,8 +155,8 @@ impl NetOptions {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use near_sdk::test_utils::{get_logs, VMContextBuilder};
-    use near_sdk::{testing_env, AccountId};
+    use near_sdk::test_utils::{ VMContextBuilder};
+    use near_sdk::{ AccountId};
 
     // part of writing unit tests is setting up a mock context
     // provide a `predecessor` here, it'll modify the default context
@@ -146,32 +167,37 @@ mod tests {
     }
 
     // TESTS HERE
-    #[test]
-    fn add_customer() {
-        let mut app = NetOptions::default();
-        app.add_customer("kenn".to_string(), "111".to_string(), "001".to_string(), "21/07/2022".to_string(), "21/08/2022".to_string(), "bronze".to_owned());
+   #[test]
+   fn add_customer(){
+    let mut  app = NetOptions::default();
+    app.add_customer("kenn".to_string(), "111".to_string(), "001".to_string(), "21/07/2022".to_string(), "21/08/2022".to_string(), "small".to_owned());
 
     assert_eq!(app.customers.len(), 1)
-    }
-    #[test]
+   }
+
+
+   #[test]
    fn update_customer(){
     let mut  app = NetOptions::default();
     
-    app.add_customer("kenn".to_string(), "111".to_string(), "001".to_string(), "21/07/2022".to_string(), "21/08/2022".to_string(), "bronze".to_owned());
-    //update Customer
-    app.update_customer("111".to_string(),"gold".to_string(),"23/06/2022".to_string(), "23/07/2022".to_string());
+    app.add_customer("Joe".to_string(), "111".to_string(), "001".to_string(), "21/07/2022".to_string(), "21/08/2022".to_string(), "bronze".to_owned());
+//update Customer
+    app.update_customer("111".to_string(),"silver".to_string(),"23/06/2022".to_string(), "23/07/2022".to_string());
 
-    let red = app.get_customer("111".to_owned());
-        match red {
-            Some(yell)=>{
-            assert_eq!(yell.net_option,NetOptionsPackages::Medium)
-            } 
-            None=>{
-            panic!("Customer Not Found")
+// get customer and check package
     
-                }    
-        }
+let red = app.get_customer("111".to_owned());
+match red {
+    Some(yell)=>{
+        assert_eq!(yell.net_option,NetOptionsPackages::Silver)
+    } 
+    None=>{
+        panic!("Customer Not Found")
     }
+}    
+
+
+   }
 
 
 }
